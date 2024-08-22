dbf83de3422fb092c3ac471d077c26e50601b25b
Ronnie <ronnielangat2020@gmail.com>
2024-08-22 16:14:41 +0300

    finished features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -74,12 +74,12 @@ const FeaturesSection = () => {
           We're happy to make you feel more comfortable in your home.
         </p>
         </div>
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10 text-white text-center items-center  ">
-          <div className="flex flex-col items-center mx-60 mt-10">
-            <Image src={paintbrush} alt="paintbrush" width={40} height={40} className="sm:w-50 sm:h-50" />
-            <div className="mt-4">
-              <h4 className="text-lg font-semibold sm:text-xl">Interior Expertise</h4>
-              <p className="leading-6 mt-2 text-sm sm:text-base">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10 text-white text-center items-center">
+          <div className="flex flex-col items-center mx-8 sm:mx-20 lg:mx-40 mt-10">
+            <Image src={paintbrush} alt="paintbrush" width={40} height={40} className="w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24" />
+            <div className="mt-6 sm:mt-8">
+              <h4 className="text-base sm:text-lg lg:text-xl font-semibold">Interior Expertise</h4>
+              <p className="leading-6 mt-2 text-sm sm:text-base lg:text-lg">
                 Expert interior design tailored to your lifestyle.
               </p>
             </div>
